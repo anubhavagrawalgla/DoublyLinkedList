01313d55fb08d2e67af008e1bb777ed3cd8f55bc
anubhavagrawalgla <anubhav.agrawal_ccv17@gla.ac.in>
2019-02-27 11:43:24 +0530

    Main method created CircularLinkedList

--- src/CircularList.java
+++ src/CircularList.java
@@ -14,5 +14,23 @@ public class CircularList {
             this.next = next;
         }
     }
+
+    int size = 0;
+    Node head = null;
+    Node tail = null;
+
+
+    public static void main(String[] args) {
+        CircularList list = new CircularList();
+        System.out.println(list);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        response.append("]");
+        return response.toString();
+    }
 }
 
